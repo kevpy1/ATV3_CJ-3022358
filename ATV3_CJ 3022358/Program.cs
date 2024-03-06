d75587d6a67c9b84e5bf4b40e8e30374163e303a
kevpy1 <kevin.c@aluno.ifsp.edu.br>
2024-03-06 08:56:34 -0300

    Adicionar arquivos de projeto.

---
+++ ATV3_CJ 3022358/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ATV3_CJ_3022358
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua nota:");
+            int grade = Convert.ToInt32(Console.ReadLine());
+
+            if (grade >= 6)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+            }
+        }
+    }
+}
